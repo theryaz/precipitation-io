1bf5ff3720b83dbc13be20752f5e0dd77a6ccd8f
Ryan Lawson <ryan.lawson437@gmail.com>
2023-07-13 17:45:37 -0400

    add more volume tests

--- test/controllers/test_rain_barrel_controller.py
+++ test/controllers/test_rain_barrel_controller.py
@@ -12,10 +12,10 @@ class TestRainBarrelController(IsolatedAsyncioTestCase):
         self.mock_rain_barrel_manifold = Manifold(
             distance_sensor=DistanceSensor(offset_cm=10, dead_zone_cm=12),
             rain_barrels=[
-                RainBarrel(diameter=22, height=32),
-                RainBarrel(diameter=22, height=32),
+                RainBarrel(diameter=55, height=95),
+                RainBarrel(diameter=55, height=95),
             ],
-            current_volume_litres=7.78,
+            current_volume_litres=0,
         )
         mock_get_rain_barrel_manifold = mock.patch(
             "rain_barrels.controllers.rain_barrels_controller.get_rain_barrel_manifold",
@@ -26,9 +26,26 @@ class TestRainBarrelController(IsolatedAsyncioTestCase):
     def tearDown(self) -> None:
         mock.patch.stopall()
 
-    async def test_get_available_water_litres(self):
+    async def test_get_available_water_litres_full(self):
+        self.mock_rain_barrel_manifold.set_volume_by_measurement(10)
         response = await self.client.get("/rain_barrels/status")
         self.assertEqual(response.status_code, 200)
         response_body = await response.get_json()
-        self.assertAlmostEqual(response_body.get("current_volume_litres", 0), 7.78, 1)
-        self.assertAlmostEqual(response_body.get("percent_full"), 32, 1)
+        self.assertAlmostEqual(response_body.get("current_volume_litres", 0), 451.4, 1)
+        self.assertAlmostEqual(response_body.get("percent_full"), 100, 1)
+
+    async def test_get_available_water_litres_empty(self):
+        self.mock_rain_barrel_manifold.set_volume_by_measurement(105)
+        response = await self.client.get("/rain_barrels/status")
+        self.assertEqual(response.status_code, 200)
+        response_body = await response.get_json()
+        self.assertAlmostEqual(response_body.get("current_volume_litres", 0), 0, 1)
+        self.assertAlmostEqual(response_body.get("percent_full"), 0, 1)
+
+    async def test_get_available_water_litres_medium(self):
+        self.mock_rain_barrel_manifold.set_volume_by_measurement(37.2)
+        response = await self.client.get("/rain_barrels/status")
+        self.assertEqual(response.status_code, 200)
+        response_body = await response.get_json()
+        self.assertAlmostEqual(response_body.get("current_volume_litres", 0), 322.16, 1)
+        self.assertAlmostEqual(response_body.get("percent_full"), 71.36, 1)
