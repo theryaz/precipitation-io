6c8a99186ba280dd20a42209f75016be06819ad4
Ryan Lawson <ryan.lawson437@gmail.com>
2023-07-13 17:54:26 -0400

    clean up data collector

--- rain_barrels/app.py
+++ rain_barrels/app.py
@@ -4,12 +4,15 @@ from rain_barrels.controllers.rain_barrels_controller import rain_barrels
 from rain_barrels.models.rain_barrel_manifold import get_rain_barrel_manifold
 from rain_barrels.models.sensor_data_collector import RainBarrelDataCollector
 
+data_collector = RainBarrelDataCollector(get_rain_barrel_manifold())
+
 app = Quart(__name__)
 
 app.register_blueprint(rain_barrels, url_prefix="/rain_barrels")
 
+app.after_serving(data_collector.stop)
+
 
 if __name__ == "__main__":
-    data_collector = RainBarrelDataCollector(get_rain_barrel_manifold())
     data_collector.start()
     app.run()
