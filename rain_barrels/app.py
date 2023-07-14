08a7212d4d1654a7e9049e91443a7658d0c6a467
Ryan Lawson <ryan.lawson437@gmail.com>
2023-07-14 09:02:51 -0400

    mock hardware modules

--- rain_barrels/app.py
+++ rain_barrels/app.py
@@ -1,4 +1,6 @@
 from quart import Quart
+from util.mock_hardware_modules import mock_hardware_modules
+mock_hardware_modules()
 
 from rain_barrels.controllers.rain_barrels_controller import rain_barrels
 from rain_barrels.models.rain_barrel_manifold import get_rain_barrel_manifold
@@ -7,11 +9,12 @@ from rain_barrels.models.lcd_display import LCDDisplay
 
 data_collector = RainBarrelDataCollector(get_rain_barrel_manifold())
 
+
 app = Quart(__name__)
 
 app.register_blueprint(rain_barrels, url_prefix="/rain_barrels")
 
-@app.after_serving()
+@app.after_serving
 def cleanup():
     print("Cleaning up threads...")
     data_collector.stop()
---
+++ rain_barrels/util/mock_hardware_modules.py
@@ -0,0 +1,11 @@
+import sys
+from unittest.mock import MagicMock
+
+def mock_hardware_modules():
+    """
+    Call when running without real hardware to mock the IO modules
+    """
+    gpio_module_mock = MagicMock()
+    sys.modules["RPi.GPIO"] = gpio_module_mock
+    smbus_module_mock = MagicMock()
+    sys.modules["smbus"] = smbus_module_mock
