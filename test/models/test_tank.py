e5be3138b3751232c245660f5e8c4a256c55ca1d
Ryan Lawson <ryan.lawson437@gmail.com>
2024-03-09 11:39:06 -0500

    fix: test setup

--- test/models/test_tank.py
+++ test/models/test_tank.py
@@ -5,10 +5,10 @@ from rain_barrels.models.tank import Tank
 class TestTank(TestCase):
 
 	def test_calculates_capacity(self):
-		tank = Tank(60, 125)
+		tank = Tank("test", 60, 125)
 		self.assertAlmostEqual(tank.capacity_litres, 353.429, 3)
 
 	def test_calculates_volume_full(self):
-		tank = Tank(60, 125)
+		tank = Tank("test", 60, 125)
 		pct = 70 / 125
 		self.assertAlmostEqual(tank.compute_volume_full(70), tank.capacity_litres * pct)
