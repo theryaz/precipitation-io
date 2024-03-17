8aa33597a9166cb168a63aed2c033a06be9be95d
Ryan Lawson <ryan.lawson@snaptravel.com>
2024-03-17 07:52:04 -0400

    refactor: plugins to class

--- plugins/button_toggle/button_toggle.py
+++ plugins/button_toggle/button_toggle.py
@@ -1,16 +1,12 @@
 from threading import Thread
+from ..plugin import Plugin
 import rain_barrels.util.use_hardware
 import RPi.GPIO as GPIO
 
-class ButtonToggle:
-    _tick_thread: Thread = None
-    _tick_thread_stop: bool = False
-
+class ButtonToggle(Plugin):
+    
     def __init__(self, config: dict, resevoir, logger):
-        print(f"[ButtonToggle] loaded with config: {config}")
-        self.config = config or {}
-        self.resevoir = resevoir
-        self.logger = logger
+        super().__init__(config, resevoir, logger)
         self._setup_pins()
     
     def _setup_pins(self):
@@ -18,22 +14,6 @@ class ButtonToggle:
         self.logger.debug(f"[ButtonToggle] Setting up button toggle on pin {self.pin}")
         GPIO.setup(self.pin, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
         GPIO.add_event_detect(self.pin, GPIO.RISING, callback=self._button_pressed, bouncetime=200)
-
-    def start(self):
-        if self._tick_thread is None:
-            self._tick_thread_stop = False
-            self_tick_thread = Thread(target=self._tick)
-            self_tick_thread.start()
-
-    def stop(self):
-        if self._tick_thread is not None:
-            self._tick_thread_stop = True
-            self._tick_thread.join()
-            self._tick_thread = None
-
-    def _tick(self):
-        while not self._tick_thread_stop:
-            pass
         
     def _button_pressed(self, channel):
         self.logger.debug(f"[ButtonToggle] Button pressed!")
---
+++ plugins/plugin.py
@@ -0,0 +1,28 @@
+from threading import Thread
+from time import sleep
+
+class Plugin:
+    _run_thread: Thread = None
+    _run_thread_stop: bool = False
+
+    def __init__(self, config: dict, resevoir, logger):
+        self.config = config or {}
+        self.resevoir = resevoir
+        self.logger = logger
+
+    def start(self):
+        if self._run_thread is None:
+            self._run_thread_stop = False
+            self_run_thread = Thread(target=self._run)
+            self_run_thread.start()
+
+    def stop(self):
+        if self._run_thread is not None:
+            self._run_thread_stop = True
+            self._run_thread.join()
+            self._run_thread = None
+            
+    @property
+    def should_stop(self):
+        return self._run_thread_stop
+
